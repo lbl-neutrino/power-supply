0242bbbc3387c353a217a7b44914df2544f8b63a
colinripley <cripley@lbl.gov>
2024-10-16 17:47:48 -0700

    Add files via upload

--- larpix_monitor_vac_pressure.py
+++ larpix_monitor_vac_pressure.py
@@ -9,58 +9,6 @@ import convert_resistance_to_temperature as ct
 # tabulate formats tabled output which we'll use to check register settings
 from tabulate import tabulate
 
-# set up the spi (i.e. the mechanism to communicate with the device)
-def set_up_spi():
-
-    bus = 0                         # only SPI bus 0 is available
-    device = 0                      # chip select pin (either 0 or 1)
-    spi.open(bus, device)           # open the specified connection
-    spi.max_speed_hz = 50000       # set SPI speed
-    spi.mode = 3 # Mode 3 samples on falling edge, shifts out on rising edge
-
-# set the AD7124-8 regsiters for our purposes
-def init_registers():
-
-    # set the data rate in the filter_0 register
-    address = 0x21                     # register 33 is filter_0
-    if data_rate == 'high':            # sets the data rate for setup0
-        msg = [address+write*64, 0, 0, 1] 
-        registers[address][3] = '- high data rate = 19k Samples/second -'
-    elif data_rate == 'medium':
-        msg = [address+write*64, 0, 0, 0b0111_1000]
-        registers[address][3] = '- medium data rate = 160 Samples/sec --'
-    elif data_rate == 'low':
-        msg = [address+write*64, 0, 0, 0b1111_1111]
-        registers[address][3] = '- low data rate = 9.4k Samples/sec -'
-    else:
-        registers[address][3] = f'data_rate setting ({data_rate}) was not a valid choice'
-    spi.xfer2(msg)
-
-    # enable Channel 0: Ain1 (positive) Ain0 (negative): Cryo Top 
-    address = 0x9                         # address 9 is channel 0
-    msg = [address + write*64, 0b1000_0000, 0b0010_0000]
-    spi.xfer2(msg)
-    registers[address][3] = '- enabled w setup0, pos=Ain1, neg=Ain0 -'
-
-    # set current out to Ain7 using an excitation current of 50 micro-A
-    address = 3                     # register 3 is IO_Control_1
-    msg = [address + write*64, 0, 0b0000_0001, 0b0000_0111]
-    spi.xfer2(msg)
-    registers[address][3] = f'- Ain7 output, ex current = 50 microA -'
-
-    # set  bipolar OFF, enable buffers for Ain(+/-) and Refin(+/-), 
-    # set Ref source = REFIN1(+/-), gain = 8
-    address = 0x19                    # register 25 is Config_0
-    msg = [address + write*64, 0b0000_0001, 0b1110_0011] 
-    spi.xfer2(msg)
-    registers[address][3] = '- Gain = 8, bipolar OFF, buffers, REFIN1(+/-) -'
-
-    # set the power mode in the ADC_CONTROL register
-    address = 1                     # register 1 is ADC_CONTROL
-    msg = [address+write*64, 0, 0b1100_0000] # Chooses power=full
-    spi.xfer2(msg)
-    registers[address][3] = '------ Power = Full ------'
-
 
 ######################################################################
 # Initialize global variables
@@ -135,18 +83,64 @@ registers = [
     ["GAIN_6", 24, 0x000001, ''],
     ["GAIN_7", 24, 0x000001, '']]
 
-######################################################################
-# Initialize global assets
-######################################################################
 
-# set up the ability to read and write to the registers. Reset all registers
-set_up_spi()
+# set up the spi (i.e. the mechanism to communicate with the device)
+def set_up_spi():
 
-# reset all adc registers to their default values
-msg = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-spi.xfer2(msg)
+    bus = 0                         # only SPI bus 0 is available
+    device = 0                      # chip select pin (either 0 or 1)
+    spi.open(bus, device)           # open the specified connection
+    spi.max_speed_hz = 50000       # set SPI speed
+    spi.mode = 3 # Mode 3 samples on falling edge, shifts out on rising edge
 
+# set the AD7124-8 regsiters for our purposes
+def init_registers():
+    # set up the ability to read and write to the registers. Reset all registers
+    set_up_spi()
 
+    # reset all adc registers to their default values
+    msg = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+    spi.xfer2(msg)
+
+    # set the data rate in the filter_0 register
+    address = 0x21                     # register 33 is filter_0
+    if data_rate == 'high':            # sets the data rate for setup0
+        msg = [address+write*64, 0, 0, 1] 
+        registers[address][3] = '- high data rate = 19k Samples/second -'
+    elif data_rate == 'medium':
+        msg = [address+write*64, 0, 0, 0b0111_1000]
+        registers[address][3] = '- medium data rate = 160 Samples/sec --'
+    elif data_rate == 'low':
+        msg = [address+write*64, 0, 0, 0b1111_1111]
+        registers[address][3] = '- low data rate = 9.4k Samples/sec -'
+    else:
+        registers[address][3] = f'data_rate setting ({data_rate}) was not a valid choice'
+    spi.xfer2(msg)
+
+    # enable Channel 0: Ain1 (positive) Ain0 (negative): Cryo Top 
+    address = 0x9                         # address 9 is channel 0
+    msg = [address + write*64, 0b1000_0000, 0b0010_0000]
+    spi.xfer2(msg)
+    registers[address][3] = '- enabled w setup0, pos=Ain1, neg=Ain0 -'
+
+    # set current out to Ain7 using an excitation current of 50 micro-A
+    address = 3                     # register 3 is IO_Control_1
+    msg = [address + write*64, 0, 0b0000_0001, 0b0000_0111]
+    spi.xfer2(msg)
+    registers[address][3] = f'- Ain7 output, ex current = 50 microA -'
+
+    # set  bipolar OFF, enable buffers for Ain(+/-) and Refin(+/-), 
+    # set Ref source = REFIN1(+/-), gain = 8
+    address = 0x19                    # register 25 is Config_0
+    msg = [address + write*64, 0b0000_0001, 0b1110_0011] 
+    spi.xfer2(msg)
+    registers[address][3] = '- Gain = 8, bipolar OFF, buffers, REFIN1(+/-) -'
+
+    # set the power mode in the ADC_CONTROL register
+    address = 1                     # register 1 is ADC_CONTROL
+    msg = [address+write*64, 0, 0b1100_0000] # Chooses power=full
+    spi.xfer2(msg)
+    registers[address][3] = '------ Power = Full ------'
 
 # get a new temperature reading from the ADC
 def read_tempers():
